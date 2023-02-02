7733eb71ebb673d7a834455a65a00105983e9460
Mahmoud Okasha <103761355+Mahmoud-Okasha74@users.noreply.github.com>
2023-02-02 16:35:24 +0200

    Delete Publisher_Subscriber_Nodes_tasks/psNode_task_ws/build directory

--- Publisher_Subscriber_Nodes_tasks/psNode_task_ws/build/cpp_count_hundred/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/okasha/psNode_task_ws/src/cpp_count_hundred
-# Build directory: /home/okasha/psNode_task_ws/build/cpp_count_hundred
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
