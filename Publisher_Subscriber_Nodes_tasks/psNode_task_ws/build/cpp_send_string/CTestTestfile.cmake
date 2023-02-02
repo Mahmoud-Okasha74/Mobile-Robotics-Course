f8d0ef0e22e1871d0f6856bed7181e73199ad4a0
Mahmoud-Okasha74 <mahmoudokasha.14@gmail.com>
2023-02-02 19:14:52 +0200

    The task is publisher and subscriber nodes in ps_nodes_task_ws, which contains four packages

--- Publisher_Subscriber_Nodes_tasks/psNode_task_ws/build/cpp_send_string/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/okasha/psNode_task_ws/src/cpp_send_string
-# Build directory: /home/okasha/psNode_task_ws/build/cpp_send_string
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Publisher_Subscriber_Nodes_tasks/psNode_task_ws/build/sendString/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/okasha/psNode_task_ws/src/sendString
-# Build directory: /home/okasha/psNode_task_ws/build/sendString
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
